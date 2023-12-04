4441e1b702c18379b8702c7c0919bc4f0d7cd390
k0d1kk <leonmiy07@gmail.com>
2023-12-04 19:00:53 +0200

    add header

---
+++ components/Header/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.headerContainer}>
+        <div className={styles.wrapper}>
+          <div className={styles.logo}>
+            <img
+              src="https://antikinoteatr.com/sites/default/files/bizonoff_site_info/logo_web_2.png"
+              alt=""
+            />
+          </div>
+          <ul className={styles.ul}>
+            <a href="">
+              <li>Залы</li>
+            </a>
+            <a href="">
+              <li>Меню</li>
+            </a>
+            <a href="">
+              <li>Правила</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
