c82c51d0eade79955117c83d56885d1d6728f545
k0d1kk <leonmiy07@gmail.com>
2023-12-04 18:31:57 +0200

    settings

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 }
- 
+
 module.exports = nextConfig
- 
