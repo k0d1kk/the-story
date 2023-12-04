4c42958f963a94997ff609b6efad0ea74e4642b2
k0d1kk <leonmiy07@gmail.com>
2023-12-04 19:22:23 +0200

    some changes

---
+++ pages/layout.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import './globals.scss';
+import type { Metadata } from 'next';
+import Head from 'next/head';
+
+export const metadata: Metadata = {
+  title: 'Бруно Кино',
+  description: '',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="uk">
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body>{children}</body>
+    </html>
+  );
+}
