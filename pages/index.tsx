7ba21cef9ed42dde4ab768e8db92838bed18d492
k0d1kk <leonmiy07@gmail.com>
2023-12-05 17:17:55 +0200

    add why

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Banner from '@/components/Banner';
 import Header from '@/components/Header';
+import Why from '@/components/Why';
 import React from 'react';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header />
       <Banner />
+      <Why />
     </>
   );
 }
